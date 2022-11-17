b76424af49ef361a4929d7cc3d3178fbdb54a03d
JakubSzczerba <jakszczerb@gmail.com>
2022-11-17 20:46:22 +0100

    git init.

---
+++ src/Controller/AppController.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file was created by Jakub Szczerba
+ * Contact: https://www.linkedin.com/in/jakub-szczerba-3492751b4/
+*/
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AppController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function homepage()
+    {       
+        
+        return $this->render('Map//loadMap.html.twig', []);
+    }
+   
+}
